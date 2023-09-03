3ffb14fbb0e4ec06cf14c593f383e878eda2ba3e
LucasHartman <lucas.hartman@kognitive.ca>
2023-09-02 20:47:25 -0400

    Attempt to fix bug

--- app.py
+++ app.py
@@ -130,19 +130,19 @@ def game_next():
     user_index = data.get('user-index')
     
     # Store response data in MongoDB
-    response_data = {
-        "index": session['index'],
-        "time": time,
-        "numOfErrors": num_of_errors,
-        "user-ID": user_ID,
-        "user-index": user_index
-    }
+    # response_data = {
+    #     "index": session['index'],
+    #     "time": time,
+    #     "numOfErrors": num_of_errors,
+    #     "user-ID": user_ID,
+    #     "user-index": user_index
+    # }
     ### Add "target": "Blue Triangle" to give indicator ###
 
     #responses_collection.insert_one(response_data)
 
     # Update index
-    user_index += 1
+    # user_index += 1
     
     next_image = get_data(user_index)
 
