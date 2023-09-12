39a0a97ad845e6ad8183892e640b2935883b90eb
Chandan <cchandel@uwo.ca>
2023-09-12 11:43:36 -0400

    test route v4

--- app.py
+++ app.py
@@ -66,8 +66,8 @@ def get_classification_data():
     with open("./static/classification/Images/image{}.png".format(index), "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
     with open("./static/classification/Labels/image{}.txt".format(index), 'r') as file:
-        #line = file.read().split()
-        present = 'present' #int(line[0]) == 1
+        line = file.read().split()
+        present = line
     if first:
         data = {
             "image": encoded_string,
