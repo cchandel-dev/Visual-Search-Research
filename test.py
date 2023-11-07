a17a4195526e0f272da1de68df07723c03c6aa2b
Chandan <cchandel@uwo.ca>
2023-11-06 19:29:07 -0500

    making sure the split is valid

--- test.py
+++ test.py
@@ -4,9 +4,10 @@ with open("./static/classification/Images/image{}.png".format(index), "rb") as i
     encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
 with open("./static/classification/Labels/image{}.txt".format(index), 'r') as file:
     line = file.read().split()
-    print(line[0][1])
     present = int(line[0][1]) == 1
     num_shapes = int(line[1])
     conjunction = bool(line[2])
     target_color = line[3]
     target_shape = line[4]
+    print(line)
+    print(present, num_shapes, conjunction, target_color, target_shape)
