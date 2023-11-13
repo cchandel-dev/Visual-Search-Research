0214086885721f2288384a20d28bf2a98a1a91b5
LucasHartman <lucas.hartman@kognitive.ca>
2023-11-13 14:54:29 -0500

    Add backend pausing

--- app.py
+++ app.py
@@ -176,6 +176,9 @@ def game_next():
         else:
             next_image = get_classification_data(user_index)
 
+        if user_index == 3:
+            next_image["pause"] = True
+
         if user_index == 0:
             next_image["max_images"] = 103
 
